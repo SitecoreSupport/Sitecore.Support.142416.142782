f926c2795d51af8e9aee6d857e5c3ea57aa8b21e
Paul Stupka <psk@sitecore.net>
2017-01-12 15:39:56 +0200

    Added fix for 142782

--- src/Sitecore.Support.142416/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.142416/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sitecore.Support.142416")]
 [assembly: AssemblyProduct("Sitecore.Support.142416")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyDescription("Call to UserProfile.IsAdministrator property can cause System.InvalidCastException. The exception is handled and does not affect functionality.")]
+[assembly: AssemblyDescription("Call to UserProfile.IsAdministrator property can cause System.InvalidCastException. Access to uninitialized property of a virtual user can cause System.Collections.Generic.KeyNotFoundException. These exceptions are handled and do not affect functionality.")]
 
--- src/Sitecore.Support.142416/Security/UserProfile.cs
+++ src/Sitecore.Support.142416/Security/UserProfile.cs
@@ -1,16 +1,72 @@
 ﻿namespace Sitecore.Support.Security
 {
+    using System;
     using System.Reflection;
+    using Sitecore.Caching.UserProfile;
+    using Sitecore.Configuration;
     using Sitecore.Diagnostics;
     using Sitecore.StringExtensions;
 
     public class UserProfile : Sitecore.Security.UserProfile
     {
         private static readonly MethodInfo IsUserVirtualMethodInfo;
+        private static readonly MethodInfo GetPropertyValueFromProfileItemMethodInfo;
+        private static readonly MethodInfo CacheGetterMethodInfo;
 
         static UserProfile()
         {
             IsUserVirtualMethodInfo = typeof(Sitecore.Security.UserProfile).GetMethod("IsUserVirtual", BindingFlags.Instance | BindingFlags.NonPublic);
+            GetPropertyValueFromProfileItemMethodInfo = typeof(Sitecore.Security.UserProfile).GetMethod("GetPropertyValueFromProfileItem", BindingFlags.Instance | BindingFlags.NonPublic);
+            CacheGetterMethodInfo = typeof(Sitecore.Security.UserProfile).GetProperty("Cache", BindingFlags.Instance | BindingFlags.NonPublic).GetGetMethod(true);
+        }
+
+        /// <summary>
+        /// Gets a named property value.
+        /// </summary>
+        /// <param name="propertyName">
+        /// Name of the property.
+        /// </param>
+        /// <returns>
+        /// The value.
+        /// </returns>
+        [CanBeNull]
+        protected override object GetPropertyValueCore([NotNull] string propertyName)
+        {
+            Assert.ArgumentNotNullOrEmpty(propertyName, "propertyName");
+
+            UserProfileCacheRecord cacheValue = ((UserProfileCache)CacheGetterMethodInfo.Invoke(this, new object[0])).GetRecord(this.UserName, propertyName);
+            if (cacheValue != null)
+            {
+                return cacheValue.Value;
+            }
+
+            object value;
+
+            try
+            {
+                if (IsUserVirtualMethodInfo != null && (bool)IsUserVirtualMethodInfo.Invoke(this, new object[0]))
+                {
+                    value = this.ProfileUser.RuntimeSettings.Properties.ContainsKey(propertyName) ? this.ProfileUser.RuntimeSettings.Properties[propertyName] : null;
+                }
+                else
+                {
+                    value = Factory.GetRetryer().Execute(() => this.GetPropertyValue(propertyName));
+                }
+            }
+            catch
+            {
+                value = null;
+            }
+
+            if ((value == null || (value is string && value.ToString() == string.Empty)) &&
+              !propertyName.Equals("ProfileItemId", StringComparison.OrdinalIgnoreCase))
+            {
+                value = (string)GetPropertyValueFromProfileItemMethodInfo.Invoke(this, new object[] { propertyName });
+            }
+
+            ((UserProfileCache)CacheGetterMethodInfo.Invoke(this, new object[0])).AddRecord(this.UserName, propertyName, value);
+
+            return value;
         }
 
         /// <summary>
